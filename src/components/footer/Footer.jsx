09a0143aee721c59208ace48dfa3264ac9baada3
miraclemichael018 <miraclemichael018@gmail.com>
2023-05-25 07:42:57 +0000

    some changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import rydgo_logo from "../../assets/images/rydgo_logo.png";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+	return (
+		<section className="footer__container">
+			<div className="footer__wrapper">
+				<div className="footer__img">
+					<img src={rydgo_logo} alt="rydgo_logo" />
+				</div>
+				
+			</div>
+		</section>
+	);
+};
 
-export default Footer
+export default Footer;
