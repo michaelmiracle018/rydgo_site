8f49a67c9ebf789eb19330dab4238a9596958715
miraclemichael018 <miraclemichael018@gmail.com>
2023-06-14 05:19:52 +0000

    some changes made on build folder

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -31,7 +31,7 @@ import RydDrop from "../pages/ryddropPage/RydDrop";
 import RydSparkle from "../pages/rydsparklePage/RydSparkle";
 import RydLaundry from "../pages/rydlaundryPage/RydLaundry";
 import RydWell from "../pages/rydwellPage/RydWell";
-import RydPharm from "../pages/rydpharmPage/RydPharm";
+import RydPharm from "../pages/RydpharmPage/RydPharm";
 import RydTour from "../pages/rydtourPage/RydTour";
 
 //PARTNER Routes Pages
@@ -49,44 +49,44 @@ import RydTuesday from "../pages/rydTuesdayPage/RydTuesday";
 
 const RoutesPage = () => {
   return (
-    <div>
-      <Routes>
-        <Route>
-          <Route path="/" element={<Home to="/" />}></Route>
-          <Route path="/driver" element={<Driver />}></Route>
-          <Route path="/rider" element={<RidersPage />}></Route>
-          <Route path="/rides" element={<Ride />}></Route>
-          <Route path="/taxi" element={<Taxi />}></Route>
-          <Route path="/bike" element={<Bike />}></Route>
-          <Route path="/rental" element={<CarRental />}></Route>
-          <Route path="/schoolRide" element={<SchoolRIde />}></Route>
-          <Route path="/driverSignUp" element={<DriverSignUp />}></Route>
-          <Route path="/rydpay" element={<Pay />}></Route>
-          <Route path="/rydgive" element={<Donate />}></Route>
-          <Route path="/dineout" element={<Dine />}></Route>
-          <Route path="/food" element={<Food />}></Route>
-          <Route path="/rydgro" element={<RydGro />}></Route>
-          <Route path="/rydsuper" element={<RydSuper />}></Route>
-          <Route path="/ryddrop" element={<RydDrop />}></Route>
-          <Route path="/rydsparkle" element={<RydSparkle />}></Route>
-          <Route path="/rydlaundry" element={<RydLaundry />}></Route>
-          <Route path="/rydwell" element={<RydWell />}></Route>
-          <Route path="/rydpharm" element={<RydPharm />}></Route>
-          <Route path="/rydtour" element={<RydTour />}></Route>
-          <Route path="/rydgo-for-business" element={<Corporate />}></Route>
-          <Route
-            path="/restaurant-partner-signup"
-            element={<Restaurant />}
-          ></Route>
-          <Route path="/rydgo-express" element={<B2B />}></Route>
-          <Route path="/about-us" element={<AboutUs />}></Route>
-          <Route path="/rydgo-social-impact" element={<SocialImpact />}></Route>
-          <Route path="/rydgo-engineer" element={<Engineer />}></Route>
-          <Route path="/rydgo-tuesday" element={<RydTuesday />}></Route>
-        </Route>
-      </Routes>
-    </div>
-  );
+		<div>
+			<Routes>
+				<Route>
+					<Route path="/" element={<Home to="/" />}></Route>
+					<Route path="/driver" element={<Driver />}></Route>
+					<Route path="/rider" element={<RidersPage />}></Route>
+					<Route path="/rides" element={<Ride />}></Route>
+					<Route path="/taxi" element={<Taxi />}></Route>
+					<Route path="/bike" element={<Bike />}></Route>
+					<Route path="/rental" element={<CarRental />}></Route>
+					<Route path="/schoolRide" element={<SchoolRIde />}></Route>
+					<Route path="/driverSignUp" element={<DriverSignUp />}></Route>
+					<Route path="/rydpay" element={<Pay />}></Route>
+					<Route path="/rydgive" element={<Donate />}></Route>
+					<Route path="/dineout" element={<Dine />}></Route>
+					<Route path="/food" element={<Food />}></Route>
+					<Route path="/rydgro" element={<RydGro />}></Route>
+					<Route path="/rydsuper" element={<RydSuper />}></Route>
+					<Route path="/ryddrop" element={<RydDrop />}></Route>
+					<Route path="/rydsparkle" element={<RydSparkle />}></Route>
+					<Route path="/rydlaundry" element={<RydLaundry />}></Route>
+					<Route path="/rydwell" element={<RydWell />}></Route>
+					<Route path="/rydpharm" element={<RydPharm />}></Route>
+					<Route path="/rydtour" element={<RydTour />}></Route>
+					<Route path="/rydgo-for-business" element={<Corporate />}></Route>
+					<Route
+						path="/restaurant-partner-signup"
+						element={<Restaurant />}
+					></Route>
+					<Route path="/rydgo-express" element={<B2B />}></Route>
+					<Route path="/about-us" element={<AboutUs />}></Route>
+					<Route path="/rydgo-social-impact" element={<SocialImpact />}></Route>
+					<Route path="/rydgo-engineer" element={<Engineer />}></Route>
+					<Route path="/rydgo-tuesday" element={<RydTuesday />}></Route>
+				</Route>
+			</Routes>
+		</div>
+	);
 };
 
 export default RoutesPage;
